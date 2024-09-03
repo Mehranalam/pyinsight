2819fc2fa1d4878d7cfcc9155396e8b98bf5af8a
Mehran Alam Beigi <Mehraxxn@gmail.com>
2024-09-03 15:34:31 +0330

    Update __init__.py

--- pyinsight/__init__.py
+++ pyinsight/__init__.py
@@ -1,5 +1,4 @@
 from .interactive import InteractiveTable, InteractivePlot
 from .text_analysis import TextAnalysis
 from .live_data import LiveDataPlot
-
-__all__ = ["InteractiveTable", "InteractivePlot", "TextAnalysis", "LiveDataPlot"]
+from .summary import QuickSummary 
